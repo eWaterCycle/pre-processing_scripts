64f97a3d4888fe4122e746e537764a83353ab720
jeromaerts <42966049+jeromaerts@users.noreply.github.com>
2019-10-16 10:51:12 +0200

    Updated coarse_bounding_box.py
    Removed duplicate code and modified print statements

--- coarse_bounding_box.py
+++ coarse_bounding_box.py
@@ -8,23 +8,14 @@ def coarse_bounding_box(shapefile, degree_buffer=float):
     
     xmin, ymin, xmax, ymax = shape.bounds
     
-    print('xmin: ' + str(xmin))
-    print('xmax: ' + str(xmax))
-    print('ymin: ' + str(ymin))
-    print('ymax: ' + str(ymax))
-
-    if xmin > 0:
-        xmin_round = math.floor(xmin) - degree_buffer
-        xmax_round = math.ceil(xmax)  + degree_buffer
-    else:
-        xmin_round = math.floor(xmin) - degree_buffer
-        xmax_round = math.ceil(xmax)  + degree_buffer
-        
-    if ymin > 0:
-        ymin_round = math.floor(ymin) - degree_buffer
-        ymax_round = math.ceil(ymax)  + degree_buffer
-    else:
-        ymin_round = math.floor(ymin) - degree_buffer  
-        ymax_round = math.ceil(ymax)  + degree_buffer 
+    xmin_round = math.floor(xmin) - degree_buffer
+    xmax_round = math.ceil(xmax)  + degree_buffer
+    ymin_round = math.floor(ymin) - degree_buffer
+    ymax_round = math.ceil(ymax)  + degree_buffer
+    
+    print('xmin: ' + str(xmin) + ' -> xmin margin: ' + str(xmin_round))
+    print('ymin: ' + str(ymin) + ' -> ymin margin: ' + str(ymin_round))
+    print('xmax: ' + str(xmax) + ' -> xmax margin: ' + str(xmax_round))
+    print('ymax: ' + str(ymax) + ' -> ymax margin: ' + str(ymax_round))
     
     return xmin_round, ymin_round, xmax_round, ymax_round
